d2e3c34f710177d5dfa991cab8b554ec632fad85
TylerChen1200 <TylerChen1200@gmail.com>
2024-08-10 21:56:19 -0700

    amplify backend changes

--- backend/server.js
+++ backend/server.js
@@ -128,6 +128,10 @@ const traitThresholds = {
 };
 
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 function selectRandom(arr, num) {
   const shuffled = arr.sort(() => 0.5 - Math.random());
   return shuffled.slice(0, num);
